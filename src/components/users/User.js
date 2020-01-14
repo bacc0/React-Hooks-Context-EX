04f929a602d4cd8c7d3c7f4b0d854b71ad5530df
Veselin Trayanov <bacco7@me.com>
2020-01-14 20:47:09 +0000

    User page UI

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 class User extends Component {
 
@@ -10,17 +13,13 @@ class User extends Component {
            
             const { 
                   bio,
-                  name,
                   blog,
-                  login,
+                  name,
+                  company,
+                  html_url,
                   hireable,
                   location,
-                  html_url,
-                  followers,
-                  following,
-                  avatar_url,
-                  public_repos,
-                  public_gists
+                  avatar_url
 
                   } = this.props.user;
 
@@ -28,9 +27,48 @@ class User extends Component {
            
 
             return (
-                  <div>
-                      {name}
-                  </div>
+                  <Fragment>
+                      <Link to = '/' className = 'btn btn-light' >Back to Search</Link>
+
+                      Hireable: {' '}
+                      { hireable ? <span className = 'text-success'>YES</span>  
+                                 : <span className = 'text-danger' >NOT</span> }
+
+                        <div className = 'card grid-2'>
+                              <div className = 'all-center'>
+
+                                    <img  src = {avatar_url} 
+                                          className = 'round-img' 
+                                          alt       ='avatar'
+                                          style     = {{ width: '125px'}}
+                                    />
+                                    <h1>{name}</h1>
+                                    <p>{location}</p>
+                              </div> 
+                              <div>
+                                    {bio && <Fragment>
+                                                <h3>Bio</h3>
+                                                <p>{bio}</p>
+                                            </Fragment>}
+                                          <a href = { html_url} className = 'btn btn-dark my-1' target = 'blank'> 
+                                                Visit Github Profile
+                                          </a>
+                                          <ul>
+                                                <li>
+                                                      {blog && <Fragment>
+                                                            Website: <strong>{blog}</strong>
+                                                               </Fragment>}
+                                                </li>
+                                                <li>
+                                                      {company && <Fragment>
+                                                            Company: <strong>{company}</strong>
+                                                               </Fragment>}
+                                                </li>
+                                          </ul>
+                              </div>
+                        </div>  
+
+                  </Fragment>
             )
       }
 }
